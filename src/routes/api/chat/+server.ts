0408f9728e72f98d9319fba51853959f0d53bdbe
Mori Adrian Rosland <mori@live.no>
2023-11-09 22:29:52 +0100

    Refactored and fixed chat API endpoint

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -1,5 +1,28 @@
-import type { RequestHandler } from './$types';
+import type { RequestEvent, RequestHandler } from './$types';
+import { OPENAI_API_KEY } from '$env/static/private';
+import { OpenAI } from 'openai';
+import { error, json } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async () => {
-    return new Response();
+
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+export const GET: RequestHandler = async (e: RequestEvent) => {
+
+    let user = { admin: true };
+
+    if (!user.admin) {
+        throw error(401, 'Unauthorized');
+    }
+
+    try {
+        const response = await openai.chat.completions.create({
+            messages: [{ role: "system", content: "You are a helpful assistant." }],
+            model: "gpt-3.5-turbo",
+        });
+
+        return json(response.choices[0].message.content);
+
+    } catch (err) {
+        throw error(500, 'Failed to fetch Data.');
+    }
 };
