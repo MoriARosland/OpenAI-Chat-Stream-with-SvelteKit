91a07dd766e040f03c55f7991bbb5885153ccc4e
Mori Adrian Rosland <mori@live.no>
2023-11-10 16:28:37 +0100

    Added DaisyUi to project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1 class="text-5xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button class="btn btn-primary">Placeholder</button>
