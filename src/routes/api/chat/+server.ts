6787f4c82ab251a26f6af3275957aa5a1c9b3964
Mori Adrian Rosland <mori@live.no>
2023-11-12 10:59:49 +0100

    Updated API to now use user inputs

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -12,15 +12,20 @@ export const POST: RequestHandler = async ({ request }) => {
 
     try {
         requestData = await request.json();
-
     } catch (err) {
         console.error('Error parsing request body:', err);
         throw error(400, 'Invalid JSON in request body.');
     }
 
     try {
+
+        const { inputText } = requestData;
+
+        // const messageArray = [{ role: "system", content: "You are a helpful assistant." }];
+        // messageArray.push({ role: "user", content: inputText });
+
         const response = await openai.chat.completions.create({
-            messages: [{ role: "system", content: "You are a helpful assistant." }],
+            messages: [{ role: "system", content: "You are a helpful assistant." }, { role: "user", content: inputText }],
             model: "gpt-3.5-turbo",
         });
 
