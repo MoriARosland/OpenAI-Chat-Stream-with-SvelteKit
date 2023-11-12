813b5ed53780f57f46d570f9fc2e95ac6f019b21
Mori Adrian Rosland <mori@live.no>
2023-11-12 10:33:10 +0100

    Added Custom Spinner Class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        emojiSpinner: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        }
+      },
+      animation: {
+        emojiSpinner: 'emojiSpinner 1s ease-in-out infinite',
+      }
+    },
   },
   plugins: [require('daisyui')],
 }
